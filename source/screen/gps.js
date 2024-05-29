ae27aae1f84d3ee337df225e25fc335a6d17aac9
YeonsuJI <yeonsu5362@gmail.com>
2024-05-29 17:25:26 +0900

    Revert "지도 확대/축소 및 내 위치 찾기 수정, 바텀시트 오류 수정"
    This reverts commit 28fd2503089be3c06e43ed824661c407c5610d91.

--- source/screen/gps.js
+++ source/screen/gps.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   StyleSheet,
   Dimensions,
   PermissionsAndroid,
   Platform,
-  TouchableOpacity,
-  Text,
 } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const windowWidth = Dimensions.get('window').width;
@@ -17,9 +15,6 @@ const windowHeight = Dimensions.get('window').height;
 
 const Gps = () => {
   const [location, setLocation] = useState(null);
-  const [zoomLevel, setZoomLevel] = useState(0); // 현재 줌 레벨을 추적
-  const mapRef = useRef(null);
-  const previousRegion = useRef(null); // 이전 지도 위치 저장
 
   useEffect(() => {
     const requestLocationPermission = async () => {
@@ -41,7 +36,7 @@ const Gps = () => {
       }
       Geolocation.watchPosition(
         position => {
-          const { latitude, longitude } = position.coords;
+          const {latitude, longitude} = position.coords;
           setLocation({
             latitude,
             longitude,
@@ -52,7 +47,7 @@ const Gps = () => {
         error => {
           console.log(error.code, error.message);
         },
-        { enableHighAccuracy: true, distanceFilter: 0, interval: 5000 },
+        {enableHighAccuracy: true, distanceFilter: 0, interval: 5000},
       );
     };
 
@@ -63,56 +58,14 @@ const Gps = () => {
     };
   }, []);
 
-  const onRegionChange = (region) => {
-    previousRegion.current = region; // 지도 이동 시 이전 위치 업데이트
-  };
-
-  const zoomIn = () => {
-    if (zoomLevel < 7 && location) { // location이 null이 아닌 경우에만 실행
-      const newZoomLevel = zoomLevel + 1;
-      const newLatitudeDelta = location.latitudeDelta / Math.pow(1.5, newZoomLevel);
-      const newLongitudeDelta = location.longitudeDelta / Math.pow(1.5, newZoomLevel);
-  
-      mapRef.current.animateToRegion({
-        ...previousRegion.current, // 이전 위치로 이동
-        latitudeDelta: newLatitudeDelta,
-        longitudeDelta: newLongitudeDelta,
-      });
-  
-      setZoomLevel(newZoomLevel);
-    }
-  };
-  
-  const zoomOut = () => {
-    if (zoomLevel > -7 && location) { // location이 null이 아닌 경우에만 실행
-      const newZoomLevel = zoomLevel - 1;
-      const newLatitudeDelta = location.latitudeDelta * Math.pow(1.5, -newZoomLevel);
-      const newLongitudeDelta = location.longitudeDelta * Math.pow(1.5, -newZoomLevel);
-  
-      mapRef.current.animateToRegion({
-        ...previousRegion.current, // 이전 위치로 이동
-        latitudeDelta: newLatitudeDelta,
-        longitudeDelta: newLongitudeDelta,
-      });
-  
-      setZoomLevel(newZoomLevel);
-    }
-  };
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.mapContainer}>
           <MapView
-            ref={mapRef}
-            provider={PROVIDER_GOOGLE}
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
             style={styles.map}
-            region={location}
-            zoomEnabled={true}
-            showsCompass={true}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            onRegionChange={onRegionChange} // 지도 이동 시 호출됨
+            region={location} // initialRegion에서 region으로 변경
           >
             {location && (
               <Marker
@@ -124,14 +77,6 @@ const Gps = () => {
               />
             )}
           </MapView>
-          <View style={styles.zoomButtons}>
-            <TouchableOpacity style={styles.button} onPress={zoomIn}>
-              <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={zoomOut}>
-              <Text style={styles.buttonText}>-</Text>
-            </TouchableOpacity>
-          </View>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -151,24 +96,5 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
-  },
-  zoomButtons: {
-    position: 'absolute',
-    bottom: 50,
-    right: 10,
-    flexDirection: 'column',
-  },
-  button: {
-    backgroundColor: 'white',
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 3,
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
   },
 });
